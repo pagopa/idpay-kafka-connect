610b9e08a8044f0280288bc7c639bd240bf3a5e4
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2024-04-05 12:59:13 +0200

    IDP-2346 update livenessProbe command and script

--- execLivenessProbe.sh
+++ execLivenessProbe.sh
@@ -3,7 +3,9 @@
 ENV_PIPELINE_FILE=.devops/deploy-pipelines.yml
 env=$(cat "$ENV_PIPELINE_FILE" | grep -i 'environment:' | cut -d' ' -f2)
 
-if [[$env == 'DEV']]; then
+echo $env
+
+if [[ $env == "DEV" ]]; then
    for connector in $(curl -s -X GET https://dev01.idpay.internal.dev.cstar.pagopa.it/idpaykafkaconnect/connectors | grep -Eo '[^]",\[]+'); do
      curl -s -X GET https://dev01.idpay.internal.dev.cstar.pagopa.it/idpaykafkaconnect/connectors/$connector/status | grep FAILED | wc -l;
    done | grep -Ez '^(0[[:space:]])+$'
